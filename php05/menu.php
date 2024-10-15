835e42045168793f5d423127ef738eff9d0590ad
“Shuntaro-Akiyama” <54-9432@moegi.waseda.jp>
2024-10-15 18:04:35 +0900

    変更

---
+++ php05/menu.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="index.php">お客様情報新規登録</a>
+            <a class="navbar-brand" href="select.php">お客様情報一覧</a>
+            <a class="navbar-brand" href="user.php">営業担当者登録</a>
+            <a class="navbar-brand" href="user_select.php">営業担当者一覧</a>
+            <a class="navbar-brand" href="logout.php">ログアウト</a>
+        </div>
+    </div>
+</nav>
